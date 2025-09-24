f7b8a4861175970acd6579949b6a507761070c3b
G.M.Gerstner <gmgerstner@gmail.com>
2025-09-24 13:42:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
-import authorData from '../data/author.json';
-import booksData from '../data/books.json';
-import newsData from '../data/news.json';
+import authorData from './data/author.json';
+import booksData from './data/books.json';
+import newsData from './data/news.json';
 import testimonialsData from '../data/testimonials.json';
 
 function Home() {
